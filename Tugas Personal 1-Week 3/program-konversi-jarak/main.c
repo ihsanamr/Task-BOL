8867a2e0016ee404760e7a143f2aea612550dca4
Ihsan Amri Muyassar <143370144+ihsanamr@users.noreply.github.com>
2023-09-05 22:27:50 +0700

    Add main.c

---
+++ Tugas Personal 1-Week 3/program-konversi-jarak/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+  // Deklarasi variabel
+  float inputKm, kmToMeter, kmToCenti;
+
+  // Header
+  printf("=====================================\n");
+  printf("\tPROGRAM KONVERSI JARAK\n");
+  printf("=====================================\n");
+
+  // Pesan selamat datang
+  printf("\nSelamat datang di Program Konversi Jarak!\n");
+
+  printf("\nTekan enter untuk memulai program...");
+  getchar();
+
+  // Input user
+  printf("\n------------------------------------\n");
+  printf("Masukkan jarak dalam satuan KM: ");
+  scanf("%f", &inputKm);
+  printf("------------------------------------\n");
+
+
+  // Konversi
+  kmToMeter = (inputKm * 1000);
+  kmToCenti = (inputKm * 100000);
+
+  // Membersihkan buffer stdin
+  while (getchar() != '\n');
+
+  // Output
+  printf("\nTekan enter untuk melihat hasil konversi...\n");
+  getchar();
+  printf("-----------------------------------------------\n");
+  printf("Hasil Konversi:\n");
+  printf("%.2f KM sama dengan %d meter\n", inputKm, (int)kmToMeter);
+  printf("\nTekan enter untuk melanjutkan konversi...\n");
+  getchar();
+  printf("%.2f KM sama dengan %d centimeter\n", inputKm, (int)kmToCenti);
+  printf("-----------------------------------------------\n");
+
+  return 0;
+}
